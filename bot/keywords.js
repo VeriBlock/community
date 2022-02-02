734a466027e9762851452370572706fda88a1dea
timstall <9978739+timstall@users.noreply.github.com>
2022-02-02 14:02:52 -0600

    add keywords.js for bot

---
+++ bot/keywords.js
@@ -0,0 +1,137 @@
+{
+    "subjects": [
+        {
+            "keys": ["wvbk", "wrapped vbk"],
+            "links": [
+                "https://wiki.veriblock.org/index.php/Wrapped_VBK",
+                "https://discord.com/channels/508431141166907395/508431141733400587/929088200218132560"
+            ]
+        },
+        {
+            "keys": ["mine", "mining", "mine vbk", "pow"],
+            "links": [
+                "https://wiki.veriblock.org/index.php/How_to_Mine_VeriBlock"
+
+            ]
+        },
+        {
+            "keys": ["pop", "proof-of-proof"],
+            "links": [
+                "https://wiki.veriblock.org/index.php/HowTo_run_PoP_Miner",
+                "https://wiki.veriblock.org/index.php/BTCSQ#How_to_PoP_Mine",
+                "https://wiki.veriblock.org/index.php/PoP_Transaction_LifeCycle",
+                "https://wiki.veriblock.org/index.php/PoP_Miner_Rewards"
+            ]
+        },
+        {
+            "keys": ["altchain"],
+            "links": [
+                "https://github.com/VeriBlock/list-pop-coins/projects/1",
+                "https://wiki.veriblock.org/index.php/Altchain_List",
+                "https://wiki.veriblock.org/index.php/BTCSQ_PoP_QuickStart",
+                "https://explore.veriblock.org/altchains",
+                "https://testnet.explore.veriblock.org/altchains"
+            ]
+        },
+        {
+            "keys": ["social", "twitter"],
+            "links": [
+                "https://twitter.com/VeriBlock",
+                "https://wiki.veriblock.org/index.php/Social_Links"
+
+            ]
+        },
+        {
+            "keys": ["bounties, bounty, contribute"],
+            "links": [
+                "https://wiki.veriblock.org/index.php/Bounties",
+                "https://wiki.veriblock.org/index.php/Community_Contribution_of_the_Week",
+                "https://github.com/orgs/VeriBlock/projects/1/views/5"
+            ]
+        },
+        {
+            "keys": ["wallet"],
+            "links": [
+                "https://wiki.veriblock.org/index.php/HowTo_Create_a_wallet",
+                "https://veriblock.org/#wallet"
+            ]
+        },
+        {
+            "keys": ["spv"],
+            "links": [
+                "https://wiki.veriblock.org/index.php/HowTo_run_SPV"
+
+            ]
+        },
+        {
+            "keys": ["exchange", "exchanges"],
+            "links": [
+                "https://wiki.veriblock.org/index.php/How-to_list_on_an_Exchange",
+                "https://veriblock.org/#exchanges",
+                "https://www.coingecko.com/en/coins/veriblock#markets"
+
+            ]
+        },
+        {
+            "keys": ["api", "develop", "code"],
+            "links": [
+                "https://wiki.veriblock.org/index.php/Dashboard_API",
+                "https://wiki.veriblock.org/index.php/PoP_Miner_API",
+                "https://wiki.veriblock.org/index.php/NodeCore_HTTP_API",
+                "https://github.com/VeriBlock/samples"
+
+            ]
+        },
+        {
+            "keys": ["bfi", "vbfi"],
+            "links": [
+                "https://veriblock.org/vbfi/",
+                "https://wiki.veriblock.org/index.php/How_VeriBlock_PoP_vBFI_Protects_Altchains",
+                "https://wiki.veriblock.org/index.php/HowTo_Setup_vBFI",
+                "https://explore.veriblock.org/bfi"
+
+            ]
+        },
+        {
+            "keys": ["subsidies"],
+            "links": [
+                "https://wiki.veriblock.org/index.php/PoP_Subsidies"
+            ]
+        },
+        {
+            "keys": ["nodecore", "daemon"],
+            "links": [
+                "https://wiki.veriblock.org/index.php/NodeCore_QuickStart",
+                "https://wiki.veriblock.org/index.php/VeriBlock_Ecosystem"
+            ]
+        },
+        {
+            "keys": ["roadmap", "plan", "status"],
+            "links": [
+                "https://veriblock.org/roadmap/",
+                "https://wiki.veriblock.org/index.php/Roadmap_FAQ",
+                "https://wiki.veriblock.org/index.php/Incident_History"
+            ]
+        },
+        {
+            "keys": ["btcsq"],
+            "links": [
+                "https://veriblock.org/labs/btcsq/",
+                "https://wiki.veriblock.org/index.php/BTCSQ"
+            ]
+        },
+        {
+            "keys": ["vaif"],
+            "links": [
+                "https://wiki.veriblock.org/index.php/VAIF"
+            ]
+        },
+        {
+            "keys": ["docker"],
+            "links": [
+                "https://wiki.veriblock.org/index.php/Docker"
+            ]
+        }
+    ]
+}
+
